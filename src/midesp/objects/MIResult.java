c3b8efdd5a2c3db7e7c6c9062e1b289cbd77fa5b
FelixHeinrich <felix.heinrich@uni-goettingen.de>
2026-08-10 14:17:41 +0200

    Replaced MIResult class with a record

--- src/midesp/objects/MIResult.java
+++ src/midesp/objects/MIResult.java
@@ -1,43 +1,18 @@
 package midesp.objects;
 
-public class MIResult implements Comparable<MIResult>{
-
-	private String snp1;
-	private String snp2;
-	private double mi;
-	
-	private double mi_apc;
-	
-	public MIResult(String snp1, String snp2, double mi, double mi_apc) {
-		this.snp1 = snp1;
-		this.snp2 = snp2;
-		this.mi = mi;
-		this.mi_apc = mi_apc;
-	}
+public record MIResult(String snp1, String snp2, double mi, double mi_apc) implements Comparable<MIResult>{
 	
 	public MIResult(String snp1, String snp2, double mi) {
 		this(snp1, snp2, mi, 0.0);
 	}
-	
-	public String getSNP1() {
-		return snp1;
-	}
-	
-	public String getSNP2() {
-		return snp2;
-	}
-	
-	public double getMI() {
-		return mi;
-	}
-	
-	public double getMI_apc() {
-		return mi_apc;
-	}
-	
+
 	@Override
-	public String toString() {
-		return snp1 + " + " + snp2 + " " + mi + " " + mi_apc;
+	public int compareTo(MIResult o) {
+		int cmp = Double.compare(this.mi_apc, o.mi_apc);
+		if(cmp != 0) {
+			return cmp;
+		}
+		return Double.compare(this.mi, o.mi);
 	}
 	
 	public String toNoAPCString() {
@@ -45,19 +20,7 @@ public class MIResult implements Comparable<MIResult>{
 	}
 	
 	@Override
-	public int compareTo(MIResult o) {
-		if(this.mi_apc < o.mi_apc) {
-			return -1;
-		}
-		else if(this.mi_apc == o.mi_apc) {
-			if(this.mi < o.mi) {
-				return -1;
-			} else if(this.mi == o.mi) {
-				return 0;
-			}
-			return 1;
-		}
-		return 1;
+	public String toString() {
+		return snp1 + " + " + snp2 + " " + mi + " " + mi_apc;
 	}
-
 }
